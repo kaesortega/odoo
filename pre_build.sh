f57165a1cd649b98ae5f6b6d4f989c97dad27698
kevin angel ortega espejo <75533240+kaesortega@users.noreply.github.com>
2025-02-07 17:46:31 -0500

    Update and rename startup.sh to pre_build.sh

--- pre_build.sh
+++ pre_build.sh
@@ -8,7 +8,4 @@ sudo apt install -y build-essential python3-dev libssl-dev libffi-dev
 
 sudo install --upgrade pip 
 
-sudo pip install -r requirements.txt
 
-# Luego inicia tu aplicación (ejemplo para un entorno Flask)
-python odoo-bin -r ${{ db_user }} -w ${{ db_pass }} --addons-path=addons -d mydb --db_host=${{ db_host}}
